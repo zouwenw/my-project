9d3ad6b1954d6df0d9ae6ff6ef3d1079829fa93b
zouwen <zouwen@dhgate.com>
2023-03-09 23:19:28 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,71 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+  <div class="box">
+    <div class="title">拖动右边把手排序</div>
+    <ul class="sort-list" v-for="(item, index) in sortList" :key="index">
+      <li class="list-item">
+        <div class="item-wrap">
+          <div class="index">{{ index + 1 }}</div>
+          <div>{{ item.name }}</div>
+        </div>
+        <van-icon name="wap-nav" color="#ccc" size="20" />
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data() {
+    return {
+      sortList: [
+        {
+          name: "选项A",
+        },
+        {
+          name: "选项B",
+        },
+        {
+          name: "选项C",
+        },
+        {
+          name: "选项D",
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.box {
+  padding: 0 20px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.index {
+  width: 20px;
+  height: 20px;
+  line-height: 20px;
+  text-align: center;
+  border-radius: 50%;
+  background: #ccc;
+  color: #fff;
+  margin-right: 10px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.title {
+  color: #ccc;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #ddd;
 }
-a {
-  color: #42b983;
+.item-wrap {
+  display: flex;
+  align-items: center;
+}
+.list-item {
+  border-bottom: 1px solid #ddd;
+  height: 50px;
+  line-height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vant from 'vant';
+import 'vant/lib/index.css';
+import 'lib-flexible';
 
+Vue.use(Vant);
 Vue.config.productionTip = false
 
 new Vue({
