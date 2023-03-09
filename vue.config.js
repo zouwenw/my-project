a6ed526457c2a31056afc6a6ecec544969e0e442
zouwen <zouwen@dhgate.com>
2023-03-10 00:25:02 +0800

    commit static file

--- vue.config.js
+++ vue.config.js
@@ -1,34 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  assetsDir: 'static',
-  publicPath: process.env.NODE_ENV == 'development' ? '/' : '../apps/com.awspaas.user.apps.bpm_actions.portal/',
-  outputDir: './dist',
-  indexPath: 'index.html',
-  productionSourceMap: false,
-  lintOnSave: false,
-  filenameHashing: true,
-  css: {
-    loaderOptions: {
-      postcss: {
-        // 这是rem适配的配置  注意： remUnit在这里要根据lib-flexible的规则来配制，如果您的设计稿是750px的，用75就刚刚好。
-        plugins: [
-          require('postcss-px2rem-exclude')({
-            remUnit: 75,
-            exclude: /node_modules/i  //这是为了不影响第三库的样式，如果入写，会导致vant的样式会变小
-          })
-        ]
-      },
-    }
-  },
-  devServer: {
-    hot: true,
-    port: 8080,
-    open: false,
-    noInfo: false,
-    overlay: {
-      warnings: true,
-      errors: true,
-    },
-  }
+  transpileDependencies: true
 })
